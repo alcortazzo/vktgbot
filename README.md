86200a59da76a6e0ab99082d6a490a66f451db32
Ruslan <42177346+alcortazzo@users.noreply.github.com>
2020-05-09 23:42:32 +0300

    updated to v0.8
    improvement parsing of vk reposts
    improvement debug info
    all logs will write into `bot.log` file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# <p align="center">vktgbot v0.7
+# <p align="center">vktgbot v0.8
 
 <p align="center">Telegram Bot on Python for repost from VKontakte community pages (group, public page or event page) to Telegram Channels.
 
@@ -15,7 +15,7 @@
 |Text post with links|**Yes** |Text post with links
 |Text post with YT vids|**50/50**|Text post & preview's urls > **VK-API restrictions**
 |Text post with audios|**50/50**|Text post **without** audios > **VK-API [restrictions](https://vk.com/dev/audio)**
-|VK reposts|Yes|Text of original post + repost text
+|VK reposts|**Yes**|Post with original post & repost text ([**e. g.**]([https://i.imgur.com/FRyo80A.png](https://i.imgur.com/FRyo80A.png)))
 |Text post with polls|Not yet|Just text post for now
 
 ### In addition, bot can skip ads posts if  in `config.py`
--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@
 
 '''
 Made by @alcortazzo
-v0.7
+v0.8
 '''
 
 import time
 import urllib
 import config
+import logging
 import requests
 import eventlet
 from datetime import datetime
@@ -30,7 +31,8 @@ def getData():
         return data.json()['response']['items']
     except eventlet.timeout.Timeout:
         print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
-              '| [ERROR] Got Timeout while retrieving VK JSON data. Cancelling...')
+              '| [WARNING] Got Timeout while retrieving VK JSON data. Cancelling...')
+        logging.warning('[WARNING] Got Timeout while retrieving VK JSON data. Cancelling...')
         return None
     finally:
         timeout.cancel()
@@ -71,47 +73,50 @@ def sendPosts(items, last_id):
 
         except Exception as ex:
             print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
-                  '| [Bot] [Info] No photos in the post. {!s} in sendPosts(): {!s}'.format(
-                      type(ex).__name__, str(ex)))
+                  '| [Bot] [Info] No photos in the post [post id:{!s}].'.format(item['id']))
+            logging.info('[Bot] [Info] No photos in the post [post id:{!s}].'.format(item['id']))
 
         try:
             if item['attachments'][0]['type'] == 'video':
                 isTypePost = 'video'
                 videoUrlPreview = item['attachments'][0]['video']['image'][-1]['url']
-                print(videoUrlPreview)
         except Exception as ex:
             print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
-                  '| [Bot] [Info] No videos in the post. {!s} in sendPosts(): {!s}'.format(
-                      type(ex).__name__, str(ex)))
+                  '| [Bot] [Info] No videos in the post [post id:{!s}].'.format(item['id']))
+            logging.info('[Bot] [Info] No videos in the post [post id:{!s}].'.format(item['id']))
 
         try:
             if item['attachments'][0]['type'] == 'link':
                 isTypePost = 'link'
                 linkurl = item['attachments'][0]['link']['url']
-                print(linkurl)
         except Exception as ex:
             print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
-                  '| [Bot] [Info] No links in the post.  {!s} in sendPosts(): {!s}'.format(
-                      type(ex).__name__, str(ex)))
+                  '| [Bot] [Info] No links in the post [post id:{!s}]'.format(item['id']))
+            logging.info('[Bot] [Info] No links in the post [post id:{!s}]'.format(item['id']))
 
-        # REPOST
+        # REPOST check
         try:
             if 'copy_history' in item:
                 isRepost = True
                 if item['copy_history'][0]['text'] != '':
                     textRepost = item['copy_history'][0]['text']
         except Exception as ex:
-            print(ex)
-
+            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                  '[ERROR] {!s} in sendPosts() (RepostCheck) [post id:{!s}]: {!s}'.format(
+                      type(ex).__name__, item['id'], str(ex)))
+            logging.error('[ERROR] {!s} in sendPosts() (RepostCheck) [post id:{!s}]: {!s}'.format(
+                type(ex).__name__, item['id'], str(ex)))
 
         # send message according to post type
         if isTypePost == 'post':
-            if isRepost:
+            if not isRepost:
+                bot.send_message(config.tgChannel, item['text'])
+            elif isRepost:
                 bot.send_message(config.tgChannel, item['text'] + '\n\n*REPOST ↓*\n\n' + '_' + textRepost + '_',
                                  parse_mode='Markdown')
-            elif not isRepost:
-                bot.send_message(config.tgChannel, item['text'])
-            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Text post sent')
+            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                  '| [Bot] Text post sent [post id:{!s}]'.format(item['id']))
+            logging.info('[Bot] Text post sent [post id:{!s}]'.format(item['id']))
 
         elif isTypePost == 'photo':
             listOfPhotos = []
@@ -119,42 +124,63 @@ def sendPosts(items, last_id):
             for urlPhoto in urlsPhoto:
                 listOfPhotos.append(types.InputMediaPhoto(urllib.request.urlopen(urlPhoto).read()))
             # send messages with photos
-            if item['text'] != '':
-                bot.send_message(config.tgChannel, item['text'])
-                print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Text post sent')
+            if not isRepost:
+                if item['text'] != '':
+                    bot.send_message(config.tgChannel, item['text'])
+            elif isRepost:
+                bot.send_message(config.tgChannel, item['text'] + '\n\n*REPOST ↓*\n\n' + '_' + textRepost + '_',
+                                 parse_mode='Markdown')
+            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                  '| [Bot] Text post sent [post id:{!s}]'.format(item['id']))
+            logging.info('[Bot] Text post sent [post id:{!s}]'.format(item['id']))
             bot.send_media_group(config.tgChannel, listOfPhotos)
-            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Post with photo sent')
+            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                  '| [Bot] Post with photo sent [post id:{!s}]'.format(item['id']))
+            logging.info('[Bot] Post with photo sent [post id:{!s}]'.format(item['id']))
 
         elif isTypePost == 'video':
             # get preview from youtube video
             listOfPreviews = []
             listOfPreviews.append(types.InputMediaPhoto(urllib.request.urlopen(videoUrlPreview).read()))
             # send messages with video preview
-            if item['text'] != '':
-                bot.send_message(config.tgChannel, item['text'])
-                print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Text post sent')
+            if not isRepost:
+                if item['text'] != '':
+                    bot.send_message(config.tgChannel, item['text'])
+            elif isRepost:
+                bot.send_message(config.tgChannel, item['text'] + '\n\n*REPOST ↓*\n\n' + '_' + textRepost + '_',
+                                 parse_mode='Markdown')
+            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                  '| [Bot] Text post sent [post id:{!s}]'.format(item['id']))
+            logging.info('[Bot] Text post sent [post id:{!s}]'.format(item['id']))
             bot.send_media_group(config.tgChannel, listOfPreviews)
-            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Post with video preview sent')
+            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                  '| [Bot] Post with video preview sent [post id:{!s}]'.format(item['id']))
+            logging.info('[Bot] Post with video preview sent [post id:{!s}]'.format(item['id']))
 
         elif isTypePost == 'link':
-            if item['text'] == '':
-                bot.send_message(config.tgChannel, linkurl)
-                print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Post with links')
-            else:
-                tgPost = '{!s}{!s}'.format(item['text'], '\n\n' + linkurl)
-                bot.send_message(config.tgChannel, tgPost)
-                print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Text post with links sent')
+            if not isRepost:
+                bot.send_message(config.tgChannel, item['text'] + '\n\n' + linkurl)
+            elif isRepost:
+                bot.send_message(config.tgChannel,
+                                 item['text'] + '\n\n' + linkurl + '\n\n*REPOST ↓*\n\n' + '_' + textRepost + '_',
+                                 parse_mode='Markdown')
+            print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
+                  '| [Bot] Text post sent [post id:{!s}]'.format(item['id']))
+            logging.info('[Bot] Text post sent [post id:{!s}]'.format(item['id']))
 
 
 def checkNewPost():
     print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [VK] Started scanning for new posts')
+    logging.info('[VK] Started scanning for new posts')
     with open('last_known_id.txt', 'r') as file:
         last_id = int(file.read())
         if last_id is None:
             print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
                   '| [ERROR] Could not read from storage. Skipped iteration')
+            logging.warning('[VK] Could not read from storage. Skipped iteration')
             return
         print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Info] Last id of vk post is {!s}'.format(last_id))
+        logging.info('[Info] Last id of vk post is {!s}'.format(last_id))
     try:
         feed = getData()
         # continue if we received data
@@ -176,26 +202,35 @@ def checkNewPost():
                     file.write(str(entries[1]['id']))
                     print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
                           '| [Info] New Last id (VK) is {!s}'.format((entries[1]['id'])))
+                    logging.info('[Info] New last_id (VK) is {!s}'.format((entries[1]['id'])))
                 except KeyError:
                     file.write(str(entries[0]['id']))
                     print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
                           '| [Info] New Last id (VK) is {!s}'.format((entries[0]['id'])))
+                    logging.info('[Info] New last_id (VK) is {!s}'.format((entries[0]['id'])))
     except Exception as ex:
         print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
               '| Excepion in type {!s} in checkNewPost(): {!s}'.format(type(ex).__name__, str(ex)))
+        logging.error('Exception of type {!s} in checkNewPost(): {!s}'.format(type(ex).__name__, str(ex)))
         pass
     print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [VK] Finished scanning')
+    logging.info('[VK] Finished scanning')
     return
 
 
 if __name__ == '__main__':
+    logging.getLogger('requests').setLevel(logging.CRITICAL)
+    logging.basicConfig(format='[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s - %(message)s',
+                        level=logging.INFO, filename='bot.log', datefmt='%d.%m.%Y %H:%M:%S')
     if not config.singleStart:
         while True:
             checkNewPost()
             print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Script went to sleep for', config.timeSleep,
                   'seconds\n\n')
+            logging.info('[Bot] Script went to sleep.\n\n')
             # pause for n minutes (timeSleep in config.py)
             time.sleep(int(config.timeSleep))
     elif config.singleStart:
         checkNewPost()
         print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"), '| [Bot] Script exited.\n')
+        logging.info('[Bot] Script exited.\n')
